d64fe123a7b8d99c4f0e941e9994959e05504636
haresh <baskaranharesh@gmail.com>
2025-01-16 17:52:20 +0530

    fix! : vector store abstraction methods

--- app/services/vectordbs.py
+++ app/services/vectordbs.py
@@ -1,49 +1,77 @@
 from langchain.vectorstores import FAISS
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
+from langchain_core.retrievers import BaseRetriever
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List
 
 ### Works only for Local Storage - VectorDB Indexes ###
 #todo: comment and black style
 class BaseVectorStore(ABC):
-
-    # Instantiate vector_db_name, vector_db_path :
-    # pass by vector_db_name
-
-    @abstractmethod
-    def add_docs_to_vector_db(
+    def __init__(
             self,
             docs: List[Document],
             embeddings: Embeddings,
             vector_db_path: str
     ):
+        self.docs = docs
+        self.embeddings = embeddings
+        self.vector_db_path = vector_db_path
+
+    # Instantiate vector_db_name, vector_db_path :
+    # pass by vector_db_name
+
+    @abstractmethod
+    def get_vdb(self) -> FAISS:
+        pass
+
+    @abstractmethod
+    def add_docs_to_vector_db(
+            self
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def get_vdb_as_retriever(self) -> BaseRetriever:
         pass
 
 
-class FAISSVectorStore(BaseVectorStore):
 
-    def add_docs_to_vector_db(
+
+class FAISSVectorStore(BaseVectorStore):
+    def __init__(
             self,
-            docs: Optional[List[Document]],
+            docs: List[Document],
             embeddings: Embeddings,
             vector_db_path: str
     ):
-        if not docs:
-            return FAISS.load_local(
-                vector_db_path, embeddings, allow_dangerous_deserialization=True
-            )
+        super().__init__(
+            docs=docs,
+            embeddings=embeddings,
+            vector_db_path=vector_db_path
+        )
 
-
-        vector_store = FAISS.from_documents(docs, embeddings)
+    def add_docs_to_vector_db(self):
+        vector_store = self.get_vdb()
 
         # Add documents to the vector store
-        vector_store.add_documents(documents=docs, embedding=embeddings)
+        vector_store.add_documents(documents=self.docs, embedding=self.embeddings)
 
         # Save vector store if path is provided
-        vector_store.save_local(vector_db_path)
+        vector_store.save_local(self.vector_db_path)
 
-        return vector_store
+        return None
+
+    def get_vdb(self) -> FAISS:
+        return FAISS.load_local(
+            folder_path=self.vector_db_path,
+            embeddings=self.embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+    def get_vdb_as_retriever(self):
+        db = self.get_vdb()
+        return db.as_retriever()
 
 
 
