1c8b0bb4aa6321695211eb970a195ab7e22844ca
haresh <baskaranharesh@gmail.com>
2025-01-16 17:20:34 +0530

    style : add comments loaders.py

--- app/services/loaders.py
+++ app/services/loaders.py
@@ -7,20 +7,29 @@ from typing import List, Optional
 class BaseLoader(ABC):
     @abstractmethod
     def get_docs(self, doc_path: Optional[str] = None) -> List[Document]:
+        """
+        This Method should be Implemented as Base Document Loader
+        :param: Document(s) | DocumentPath
+        :return: List of Documents
+        """
         pass
 
 
 class PDFLoader(BaseLoader):
 
     def get_docs(self, doc_path: Optional[str] = None) -> List[Document]:
-        final_path = doc_path
+        """
+        This method should be an Implementation of BaseLoader for PDF documents
+        :param doc_path:
+        :return: Document Loader of that instance
+        """
 
-        # Ensure we have a path to work with
-        if not final_path:
+        # raise ValueError :
+        # doc_path is not passed as a parameter
+        if not doc_path:
             raise ValueError(
                 "doc_path must be provided either during initialization or method call"
             )
 
-        # Load PDF file from the given doc_path
-        loader = PyPDFLoader(final_path)
-        return loader.load()
+        # return PDF loader of this instance
+        return PyPDFLoader(doc_path).load()
