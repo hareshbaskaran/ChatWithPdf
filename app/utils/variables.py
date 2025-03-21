e5c41b06bc95277c6e262a14daabe103958981b1
haresh <haresh.b@viewzenlabs.com>
2025-03-21 14:47:58 +0530

    fix(chatpdf) : index manager path change

--- app/utils/variables.py
+++ app/utils/variables.py
@@ -10,5 +10,3 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 SQL_MANAGER_NAMESPACE = f"FAISS/chatpdf"
 SQLITE_DB_URL = "sqlite:///chatpdf_sqlmanager.sql"
 VECTOR_DB_PATH = "docs/out_data"
-
-## Config File Configurations
